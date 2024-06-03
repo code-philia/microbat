4bd6c7a7b71dd7626df2688a027e68f275ec2316
HongshuW <hongshu.wang.uni@gmail.com>
2024-06-03 16:56:49 +0800

    determine the scope of recovery

--- microbat/src/main/microbat/tracerecov/TraceRecoverer.java
+++ microbat/src/main/microbat/tracerecov/TraceRecoverer.java
@@ -10,25 +10,22 @@ import microbat.model.value.VarValue;
 public class TraceRecoverer {
 	/**
 	 * @author hongshuwang
-	 * @param rootVar 
 	 */
 	public void recoverDataDependency(Trace trace, TraceNode currentStep, VarValue targetVar, VarValue rootVar) {
-		
-		
 		List<VarValue> parents = parseParentVariables(trace, currentStep, targetVar, rootVar);
-		
+
 		/**
 		 * we might start with the root variable
 		 */
-		for(VarValue parent: parents) {
+		for (VarValue parent : parents) {
 			List<TraceNode> candidateSteps = parseCandidateReadingSteps(trace, parent);
-			for(TraceNode step: candidateSteps) {
-				if(isWritingTarget(step)) {
+			for (TraceNode step : candidateSteps) {
+				if (isWritingTarget(step)) {
 					step.addWrittenVariable(targetVar);
 				}
 			}
 		}
-		
+
 //		/* 3. Execution Simulation */
 //		/*
 //		 * Identify additional linking steps and simulate execution by calling LLM
@@ -41,7 +38,7 @@ public class TraceRecoverer {
 //		} catch (IOException ioException) {
 //			ioException.printStackTrace();
 //		}
-		
+
 	}
 
 	private List<TraceNode> parseCandidateReadingSteps(Trace trace, VarValue parent) {
@@ -49,43 +46,46 @@ public class TraceRecoverer {
 		return null;
 	}
 
-	private List<VarValue> parseParentVariables(Trace trace, TraceNode currentStep, VarValue targetVar, VarValue rootVar) {
-		
-		/* 2. Context Scope Analysis */
-		/*
-		 * Build a variable graph.
-		 */
-		VariableGraph.reset();
-		
+	private List<VarValue> parseParentVariables(Trace trace, TraceNode currentStep, VarValue targetVar,
+			VarValue rootVar) {
+
 		/* initialize graph */
-		VariableGraph.addVar(targetVar);
-		
+		VariableGraph.reset();
+		VariableGraph.addVar(rootVar);
+
 		/* determine the scope of recovery */
-		TraceNode scopeStart = trace.findDataDependency(currentStep, targetVar);
+		// search for last written step other than a return step
+		VarValue lastWrittenVariable = null;
+		TraceNode scopeStart = currentStep;
+		while (lastWrittenVariable == null) {
+			scopeStart = trace.findDataDependency(scopeStart, rootVar);
+			lastWrittenVariable = scopeStart.getWrittenVariables().stream()
+					.filter(v -> v.getVarName() != null && !v.getVarName().contains("#")).findFirst().orElse(null);
+		}
+
 		int start = scopeStart.getOrder();
 		int end = currentStep.getOrder();
-		
+
+		/* build variable graph */
 		for (int i = start; i <= end; i++) {
 			TraceNode step = trace.getTraceNode(i);
-			
+
 			List<VarValue> variables = new ArrayList<>();
 			variables.addAll(step.getReadVariables());
 			variables.addAll(step.getWrittenVariables());
-			
-			VarValue varInGraph = variables.stream()
-					.filter(v -> VariableGraph.containsVar(v))
-					.findAny().orElse(null);
+
+			VarValue varInGraph = variables.stream().filter(v -> VariableGraph.containsVar(v)).findAny().orElse(null);
 			if (varInGraph != null) {
 				/* identify relevant steps */
 				VariableGraph.addRelevantStep(step);
-				
+
 				/* variable mapping */
 				VariableGraph.mapVariable(step);
 			} else {
 				continue;
 			}
 		}
-		
+
 		return null;
 	}
 
