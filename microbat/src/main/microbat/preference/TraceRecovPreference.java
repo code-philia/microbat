604447613ff8074533427b5a1bf26682561807b8
HongshuW <hongshu.wang.uni@gmail.com>
2024-07-04 22:31:07 +0800

    add collect prompt option in TraceRecovPreference

--- microbat/src/main/microbat/preference/TraceRecovPreference.java
+++ microbat/src/main/microbat/preference/TraceRecovPreference.java
@@ -31,6 +31,7 @@ public class TraceRecovPreference extends PreferencePage implements IWorkbenchPr
 	public static final String USE_MUTATION_CONFIG = "use_mutation_config";
 	public static final String API_KEY = "api_key";
 	public static final String MODEL_TYPE = "model_type";
+	public static final String COLLECT_PROMPT = "collect_prompt";
 	public static final String ENABLE_LOGGING = "enable_logging";
 	public static final String LOG_DEBUG_INFO = "log_debug_info";
 
@@ -40,6 +41,7 @@ public class TraceRecovPreference extends PreferencePage implements IWorkbenchPr
 	private boolean isMutationExperiment;
 	private String apiKey;
 	private LLMModel llmModelType = LLMModel.GPT4O; // default model
+	private boolean isCollectingPrompt;
 	private boolean isEnableLogging;
 	private boolean logDebugInfo;
 
@@ -49,6 +51,7 @@ public class TraceRecovPreference extends PreferencePage implements IWorkbenchPr
 	private Button useMutationConfigButton;
 	private Combo modelTypeCombo;
 	private Text apiKeyText;
+	private Button isCollectingPromptButton;
 	private Button isEnableLoggingButton;
 	private Button logDebugInfoButton;
 
@@ -91,6 +94,13 @@ public class TraceRecovPreference extends PreferencePage implements IWorkbenchPr
 		String modelType = Activator.getDefault().getPreferenceStore().getString(MODEL_TYPE);
 		if (modelType != null && !modelType.equals("")) {
 			this.llmModelType = LLMModel.valueOf(modelType);
+		}
+
+		String isCollectingPromptString = Activator.getDefault().getPreferenceStore().getString(COLLECT_PROMPT);
+		if (isCollectingPromptString != null && isCollectingPromptString.equals("true")) {
+			this.isCollectingPrompt = true;
+		} else {
+			this.isCollectingPrompt = false;
 		}
 
 		String isEnableLoggingString = Activator.getDefault().getPreferenceStore().getString(ENABLE_LOGGING);
@@ -159,6 +169,9 @@ public class TraceRecovPreference extends PreferencePage implements IWorkbenchPr
 	private void createLogSettingGroup(final Composite parent) {
 		String title = "Log Settings";
 		Group logSettingGroup = initGroup(parent, title);
+
+		String collectPromptLabel = "RQ3: Collect and Label Prompts";
+		this.isCollectingPromptButton = createCheckButton(logSettingGroup, collectPromptLabel, this.isCollectingPrompt);
 
 		String enableLogLabel = "Enable Logging";
 		this.isEnableLoggingButton = createCheckButton(logSettingGroup, enableLogLabel, this.isEnableLogging);
@@ -231,6 +244,7 @@ public class TraceRecovPreference extends PreferencePage implements IWorkbenchPr
 		preferences.put(USE_MUTATION_CONFIG, String.valueOf(this.useMutationConfigButton.getSelection()));
 		preferences.put(API_KEY, this.apiKeyText.getText());
 		preferences.put(MODEL_TYPE, this.modelTypeCombo.getText());
+		preferences.put(COLLECT_PROMPT, String.valueOf(this.isCollectingPromptButton.getSelection()));
 		preferences.put(ENABLE_LOGGING, String.valueOf(this.isEnableLoggingButton.getSelection()));
 		preferences.put(LOG_DEBUG_INFO, String.valueOf(this.logDebugInfoButton.getSelection()));
 
@@ -242,6 +256,8 @@ public class TraceRecovPreference extends PreferencePage implements IWorkbenchPr
 				String.valueOf(this.useMutationConfigButton.getSelection()));
 		Activator.getDefault().getPreferenceStore().putValue(API_KEY, this.apiKeyText.getText());
 		Activator.getDefault().getPreferenceStore().putValue(MODEL_TYPE, this.modelTypeCombo.getText());
+		Activator.getDefault().getPreferenceStore().putValue(COLLECT_PROMPT,
+				String.valueOf(this.isCollectingPromptButton.getSelection()));
 		Activator.getDefault().getPreferenceStore().putValue(ENABLE_LOGGING,
 				String.valueOf(this.isEnableLoggingButton.getSelection()));
 		Activator.getDefault().getPreferenceStore().putValue(LOG_DEBUG_INFO,
