70c5071317b755889202f9f064680c7121ae39e1
HongshuW <hongshu.wang.uni@gmail.com>
2024-07-29 23:56:35 +0800

    add VarSkeletonParser to parse string into VariableSkeleton

--- microbat/src/main/microbat/tracerecov/autoprompt/DatasetReader.java
+++ microbat/src/main/microbat/tracerecov/autoprompt/DatasetReader.java
@@ -19,8 +19,45 @@ public class DatasetReader {
 	public DatasetReader() {
 		// TODO: move this to preference page
 		String variableExpansionFile = "var_expansion.txt";
-		this.variableExpansionPath = Activator.getDefault().getPreferenceStore()
-				.getString(TraceRecovPreference.PROMPT_GT_PATH) + File.separator + variableExpansionFile;
+//		this.variableExpansionPath = Activator.getDefault().getPreferenceStore()
+//				.getString(TraceRecovPreference.PROMPT_GT_PATH) + File.separator + variableExpansionFile;
+		this.variableExpansionPath = "/Users/hongshuwang/Desktop/var_expansion.txt";
+	}
+
+	/**
+	 * Reads variable expansion dataset.
+	 * 
+	 * var_name, var_type, var_value, class_structure, source_code, ground_truth
+	 */
+	public ArrayList<HashMap<String, String>> readCompleteDataset() {
+		ArrayList<HashMap<String, String>> dataset = new ArrayList<>();
+
+		try {
+			BufferedReader bufferReader = new BufferedReader(new FileReader(variableExpansionPath));
+
+			String line;
+			// read content
+			while ((line = bufferReader.readLine()) != null) {
+				String[] columns = line.split("###");
+				if (columns.length != 6) {
+					continue;
+				}
+
+				HashMap<String, String> datapoint = new HashMap<>();
+				datapoint.put("var_name", columns[0]);
+				datapoint.put("var_type", columns[1]);
+				datapoint.put("var_value", columns[2]);
+				datapoint.put("class_structure", columns[3]);
+				datapoint.put("source_code", columns[4]);
+				datapoint.put("ground_truth", columns[5]);
+
+				dataset.add(datapoint);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return dataset;
 	}
 
 	/**
---
+++ microbat/src/main/microbat/tracerecov/varskeleton/VarSkeletonParser.java
@@ -0,0 +1,68 @@
+package microbat.tracerecov.varskeleton;
+
+/**
+ * This class is used to parse a class structure string into Variable Skeleton.
+ */
+public class VarSkeletonParser {
+
+	public VarSkeletonParser() {
+	}
+
+	public VariableSkeleton parseClassStructure(String classStructure) {
+		String[] keyValuePair = classStructure.split(":", 2); // assume root layer has one key
+		String varType = keyValuePair[0];
+		String varValue = keyValuePair[1];
+
+		VariableSkeleton variableSkeleton = new VariableSkeleton(varType);
+		parseClassStructureRecur(varValue, variableSkeleton);
+
+		return variableSkeleton;
+	}
+
+	public void parseClassStructureRecur(String varValue, VariableSkeleton parentVar) {
+		if (varValue.startsWith("{")) {
+			varValue = extractVarValueContent(varValue);
+		}
+
+		int lastIndex = 0;
+		String key = "";
+		String value = "";
+		int bracketCount = 0;
+		char lastCharacter = ' ';
+		for (int i = 0; i < varValue.length(); i++) {
+			char character = varValue.charAt(i);
+			if (character == ':' && bracketCount == 0) {
+				key = varValue.substring(lastIndex, i);
+				lastIndex = i + 1;
+			} else if (character == '{') {
+				bracketCount++;
+			} else if (character == '}') {
+				bracketCount--;
+			} else if (character == ';' && bracketCount == 0) {
+				if (lastCharacter == '}') {
+					value = varValue.substring(lastIndex, i);
+
+					String[] typeNamePair = key.split(" ");
+					VariableSkeleton childVar = new VariableSkeleton(typeNamePair[0], typeNamePair[1]);
+					parentVar.addChild(childVar);
+
+					parseClassStructureRecur(value, childVar);
+				} else {
+					key = varValue.substring(lastIndex, i);
+
+					String[] typeNamePair = key.split(" ");
+					VariableSkeleton childVar = new VariableSkeleton(typeNamePair[0], typeNamePair[1]);
+					parentVar.addChild(childVar);
+				}
+				lastIndex = i + 1;
+			}
+			lastCharacter = character;
+		}
+	}
+
+	private String extractVarValueContent(String varValue) {
+		int startIndex = varValue.indexOf("{");
+		int endIndex = varValue.lastIndexOf("}");
+		return varValue.substring(startIndex + 1, endIndex);
+	}
+}
