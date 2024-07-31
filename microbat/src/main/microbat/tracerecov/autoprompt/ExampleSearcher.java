9025366bc2a879a1dbac70fc0a7ce395041a440c
HongshuW <hongshu.wang.uni@gmail.com>
2024-07-31 19:48:38 +0800

    add in-context learning experiment script

--- microbat/src/main/microbat/tracerecov/autoprompt/ExampleSearcher.java
+++ microbat/src/main/microbat/tracerecov/autoprompt/ExampleSearcher.java
@@ -1,9 +1,15 @@
 package microbat.tracerecov.autoprompt;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.function.Function;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import microbat.tracerecov.TraceRecovUtils;
+import microbat.tracerecov.executionsimulator.ExecutionSimulator;
 import microbat.tracerecov.varskeleton.VarSkeletonParser;
 import microbat.tracerecov.varskeleton.VariableSkeleton;
 
@@ -12,17 +18,20 @@ import microbat.tracerecov.varskeleton.VariableSkeleton;
  */
 public class ExampleSearcher {
 
-	private ArrayList<HashMap<String, String>> dataset;
+	private ArrayList<HashMap<String, String>> trainingDataset;
+	private ArrayList<HashMap<String, String>> testingDataset;
 	private VarSkeletonParser varSkeletonParser;
 	private PromptTemplateFiller promptTemplateFiller;
+	private ExecutionSimulator executionSimulator;
 
 	public ExampleSearcher() {
 		DatasetReader datasetReader = new DatasetReader();
-		dataset = datasetReader.readCompleteDataset();
-
+		ArrayList<ArrayList<HashMap<String, String>>> datasets = datasetReader.getTrainingAndTestingDataset();
+		trainingDataset = datasets.get(0);
+		testingDataset = datasets.get(1);
 		varSkeletonParser = new VarSkeletonParser();
-
 		promptTemplateFiller = new PromptTemplateFiller();
+		executionSimulator = new ExecutionSimulator();
 	}
 
 	public String searchForExample(HashMap<String, String> datapoint) {
@@ -34,8 +43,8 @@ public class ExampleSearcher {
 
 		double minDiffScore = 1;
 		int datapointIndex = 0;
-		for (int i = 0; i < dataset.size(); i++) {
-			HashMap<String, String> example = dataset.get(i);
+		for (int i = 0; i < trainingDataset.size(); i++) {
+			HashMap<String, String> example = trainingDataset.get(i);
 			String exampleClassStructure = example.get(classStructureKey);
 			VariableSkeleton exampleVarSkeleton = varSkeletonParser.parseClassStructure(exampleClassStructure);
 
@@ -46,8 +55,64 @@ public class ExampleSearcher {
 			}
 		}
 
-		HashMap<String, String> closestExample = dataset.get(datapointIndex);
+		HashMap<String, String> closestExample = trainingDataset.get(datapointIndex);
 		String groundTruth = TraceRecovUtils.processInputStringForLLM(closestExample.get(groundTruthKey));
 		return promptTemplateFiller.getExample(closestExample, groundTruth);
+	}
+
+	/**
+	 * For testing purpose
+	 */
+	public void recordLoss() {
+		ArrayList<Double> baselineLoss = new ArrayList<>();
+		ArrayList<Double> experimentLoss = new ArrayList<>();
+
+		for (HashMap<String, String> datapoint : testingDataset) {
+			baselineLoss.add(getLoss(datapoint, x -> promptTemplateFiller.getDefaultVariableExpansionPrompt(x)));
+			experimentLoss.add(getLoss(datapoint,
+					x -> promptTemplateFiller.getVariableExpansionPrompt(x, this.searchForExample(x))));
+		}
+		
+		LossDataCollector lossDataCollector = new LossDataCollector();
+		lossDataCollector.saveDataToFile(baselineLoss, experimentLoss);
+	}
+
+	/**
+	 * Copied from {@code AutoPromptEngineer}
+	 */
+	private String getLLMOutput(String request) {
+		String output;
+		try {
+			output = executionSimulator.sendRequest("", request);
+			int begin = output.indexOf("{");
+			int end = output.lastIndexOf("}");
+			return output.substring(begin, end + 1);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private double getLoss(HashMap<String, String> datapoint, Function<HashMap<String, String>, String> operation) {
+		JSONObject groundTruthJSON = new JSONObject(datapoint.get("ground_truth"));
+
+		String request = operation.apply(datapoint);
+		String output = getLLMOutput(request);
+
+		JSONObject outputJSON;
+		try {
+			outputJSON = new JSONObject(output);
+		} catch (JSONException jsonException) {
+			return 1;
+		}
+
+		// compute loss
+		LossCalculator lossCalculator = new LossCalculator();
+		return lossCalculator.computeLoss(outputJSON, groundTruthJSON);
+	}
+
+	public static void main(String[] args) {
+		ExampleSearcher exampleSearcher = new ExampleSearcher();
+		exampleSearcher.recordLoss();
 	}
 }
