61ed0f8b2c554c7e0dc0a306c8c6d6ba5a54d080
HongshuW <hongshu.wang.uni@gmail.com>
2024-09-23 20:26:57 +0800

    implement getVarReturnStatus method

--- microbat/src/main/microbat/tracerecov/staticverifiers/AliasRelationsVerifier.java
+++ microbat/src/main/microbat/tracerecov/staticverifiers/AliasRelationsVerifier.java
@@ -7,47 +7,50 @@ import java.util.Map;
 import org.apache.bcel.generic.ALOAD;
 import org.apache.bcel.generic.ConstantPoolGen;
 import org.apache.bcel.generic.FieldInstruction;
+import org.apache.bcel.generic.GETFIELD;
 import org.apache.bcel.generic.Instruction;
 import org.apache.bcel.generic.InvokeInstruction;
 import org.apache.bcel.generic.PUTFIELD;
 import org.apache.bcel.generic.PUTSTATIC;
+import org.apache.bcel.generic.ReturnInstruction;
 
 import microbat.codeanalysis.bytecode.CFG;
 import microbat.codeanalysis.bytecode.CFGNode;
 
 /**
  * This class performs static analysis and verifies whether a candidate variable
- * is guaranteed to be assigned / not assigned / returned / not returned by a 
+ * is guaranteed to be assigned / not assigned / returned / not returned by a
  * method call.
  * 
  * @author hongshuwang
  */
 public class AliasRelationsVerifier {
-	
+
 	private CFG cfg;
 	private ConstantPoolGen constantPoolGen;
-	
+
 	public AliasRelationsVerifier(CFG cfg) {
 		this.cfg = cfg;
 		this.constantPoolGen = new ConstantPoolGen(cfg.getConstantPool());
 	}
-	
+
 	public AssignRelation getVarAssignRelation(String varName) {
 		if (varName.contains("[") || varName.contains("]")) {
 			return AssignRelation.getNoGuaranteeAssignRelation();
 		}
 		return getAssignRelationRecur(cfg.getStartNode(), new HashMap<>(), varName);
 	}
-	
-	private AssignRelation getAssignRelationRecur(CFGNode node, Map<CFGNode, AssignRelation> visitedNodes, String varName) {
+
+	private AssignRelation getAssignRelationRecur(CFGNode node, Map<CFGNode, AssignRelation> visitedNodes,
+			String varName) {
 		visitedNodes.put(node, null);
-		
+
 		AssignRelation assignRelation = getAssignRelationAtNode(node, varName);
 		if (assignRelation.getAssignStatus().equals(AssignStatus.NO_GUARANTEE)) {
 			visitedNodes.put(node, assignRelation);
 			return assignRelation;
 		}
-		
+
 		AssignRelation assignRelationAmongChildren = null;
 		List<CFGNode> children = node.getChildren();
 		for (CFGNode child : children) {
@@ -84,8 +87,8 @@ public class AliasRelationsVerifier {
 			visitedNodes.put(node, assignRelation);
 			return assignRelation;
 		} else if (assignRelationAmongChildren.getAssignStatus().equals(AssignStatus.GUARANTEE_ASSIGN)) {
-			// if the children all have status GUARANTEE_ASSIGN, and are assigning to the same field, 
-			// then the overall status is GUARANTEE_ASSIGN
+			// if the children all have status GUARANTEE_ASSIGN, and are assigning to the
+			// same field, then the overall status is GUARANTEE_ASSIGN
 			visitedNodes.put(node, assignRelationAmongChildren);
 			return assignRelationAmongChildren;
 		}
@@ -100,7 +103,7 @@ public class AliasRelationsVerifier {
 	 */
 	private AssignRelation getAssignRelationAtNode(CFGNode node, String varName) {
 		Instruction instruction = node.getInstructionHandle().getInstruction();
-		
+
 		if (instruction instanceof ALOAD) {
 			List<CFGNode> children = node.getChildren();
 			boolean isLastNode = children == null || children.size() == 0;
@@ -133,8 +136,93 @@ public class AliasRelationsVerifier {
 	}
 
 	private ReturnStatus getReturnStatusRecur(CFGNode node, Map<CFGNode, ReturnStatus> visitedNodes, String varName) {
-		// TODO Auto-generated method stub
-		return null;
+		visitedNodes.put(node, null);
+
+		ReturnStatus returnStatus = getReturnStatusAtNode(node, varName);
+		// only check the whole graph when status is GUARANTEE_NO_RETURN
+		if (returnStatus != ReturnStatus.GUARANTEE_NO_RETURN) {
+			visitedNodes.put(node, returnStatus);
+			return returnStatus;
+		}
+
+		ReturnStatus returnStatusAmongChildren = null;
+		List<CFGNode> children = node.getChildren();
+		for (CFGNode child : children) {
+			ReturnStatus childReturnStatus;
+			if (visitedNodes.containsKey(child)) {
+				if (visitedNodes.get(child) == null) {
+					continue;
+				} else {
+					childReturnStatus = visitedNodes.get(child);
+				}
+			} else {
+				childReturnStatus = getReturnStatusRecur(child, visitedNodes, varName);
+			}
+
+			// if status is NO_GUARANTEE at any point, the overall status is NO_GUARANTEE
+			if (childReturnStatus == ReturnStatus.NO_GUARANTEE) {
+				visitedNodes.put(node, childReturnStatus);
+				return childReturnStatus;
+			}
+
+			if (returnStatusAmongChildren == null) {
+				returnStatusAmongChildren = childReturnStatus;
+			} else if (returnStatusAmongChildren != childReturnStatus) {
+				// if return status are different among branches, the overall status is
+				// NO_GUARANTEE
+				visitedNodes.put(node, ReturnStatus.NO_GUARANTEE);
+				return ReturnStatus.NO_GUARANTEE;
+			}
+		}
+
+		if (returnStatusAmongChildren == null) {
+			// if there are no children, return the status of the current node
+			visitedNodes.put(node, returnStatus);
+			return returnStatus;
+		} else if (returnStatusAmongChildren == ReturnStatus.GUARANTEE_RETURN) {
+			// if the children all have status GUARANTEE_RETURN, then the overall status is
+			// GUARANTEE_RETURN
+			visitedNodes.put(node, returnStatusAmongChildren);
+			return returnStatusAmongChildren;
+		}
+
+		visitedNodes.put(node, returnStatus);
+		return returnStatus;
 	}
-	
+
+	/**
+	 * Analyze two nodes (one return step) at a time.
+	 * Return Pattern: getfield return
+	 */
+	private ReturnStatus getReturnStatusAtNode(CFGNode node, String varName) {
+		Instruction instruction = node.getInstructionHandle().getInstruction();
+
+		if (instruction instanceof GETFIELD) {
+			GETFIELD getFieldInstruction = (GETFIELD) instruction;
+			String fieldName = getFieldInstruction.getFieldName(constantPoolGen);
+			if (!fieldName.equals(varName)) {
+				return ReturnStatus.GUARANTEE_NO_RETURN;
+			}
+
+			List<CFGNode> children = node.getChildren();
+			boolean isLastNode = children == null || children.size() == 0;
+			if (isLastNode) {
+				return ReturnStatus.GUARANTEE_NO_RETURN;
+			} else if (children.size() > 1) {
+				return ReturnStatus.NO_GUARANTEE;
+			} else {
+				Instruction nextInstruction = children.get(0).getInstructionHandle().getInstruction();
+				if (nextInstruction instanceof ReturnInstruction) {
+					return ReturnStatus.GUARANTEE_RETURN;
+				}
+			}
+		} else if (instruction instanceof InvokeInstruction) {
+			return ReturnStatus.NO_GUARANTEE;
+		} else {
+			return ReturnStatus.GUARANTEE_NO_RETURN;
+		}
+
+		return ReturnStatus.NO_GUARANTEE;
+	}
+
 }
--- microbat/src/main/microbat/tracerecov/staticverifiers/AssignRelation.java
+++ microbat/src/main/microbat/tracerecov/staticverifiers/AssignRelation.java
@@ -1,45 +1,43 @@
 package microbat.tracerecov.staticverifiers;
 
 public class AssignRelation {
-	
+
 	private AssignStatus assignStatus;
 	private String variableOfInterest;
 	private String field;
-	
+
 	private AssignRelation(AssignStatus assignStatus, String variableOfInterest, String field) {
 		this.assignStatus = assignStatus;
 		this.variableOfInterest = variableOfInterest;
 		this.field = field;
 	}
-	
+
 	public static AssignRelation getNoGuaranteeAssignRelation() {
 		return new AssignRelation(AssignStatus.NO_GUARANTEE, null, null);
 	}
-	
+
 	public static AssignRelation getGuaranteeNoAssignRelation() {
 		return new AssignRelation(AssignStatus.GUARANTEE_NO_ASSIGN, null, null);
 	}
-	
+
 	public static AssignRelation getGuaranteeAssignRelation(String variableOfInterest, String field) {
 		return new AssignRelation(AssignStatus.GUARANTEE_ASSIGN, variableOfInterest, field);
 	}
-	
+
 	@Override
 	public boolean equals(Object other) {
 		if (other instanceof AssignRelation) {
 			AssignRelation otherRelation = (AssignRelation) other;
 			boolean sameAssignStatus = this.assignStatus.equals(otherRelation.assignStatus);
-			boolean sameVariableOfInterest = this.variableOfInterest == null 
-					? otherRelation.variableOfInterest == null 
+			boolean sameVariableOfInterest = this.variableOfInterest == null ? otherRelation.variableOfInterest == null
 					: this.variableOfInterest.equals(otherRelation.variableOfInterest);
-			boolean sameField = this.field == null 
-					? otherRelation.field == null 
+			boolean sameField = this.field == null ? otherRelation.field == null
 					: this.field.equals(otherRelation.field);
 			return sameAssignStatus && sameVariableOfInterest && sameField;
 		}
 		return false;
 	}
-	
+
 	public AssignStatus getAssignStatus() {
 		return assignStatus;
 	}
@@ -51,5 +49,5 @@ public class AssignRelation {
 	public String getField() {
 		return field;
 	}
-	
+
 }
