b92ea04720a1a827793c37ef529761f7f062b28a
HongshuW <hongshu.wang.uni@gmail.com>
2025-03-13 18:20:09 +0800

    update LLM API endpoint

--- microbat/src/main/microbat/tracerecov/executionsimulator/SimulatorConstants.java
+++ microbat/src/main/microbat/tracerecov/executionsimulator/SimulatorConstants.java
@@ -18,7 +18,7 @@ public class SimulatorConstants {
 	public static final String CLAUDE35 = "claude-3.5";
 	public static final String GEMINI = "gemini-1.5-flash";
 
-	public static final String GPT_API_ENDPOINT = "https://api.openai.com/v1/chat/completions";
+	public static final String GPT_API_ENDPOINT = "https://api.key77qiqi.cn/v1/chat/completions";
 	public static final String CLAUDE3_API_ENDPOINT = "https://api.claude.ai/v3";
 	public static final String CLAUDE35_API_ENDPOINT = "https://api.claude.ai/v3.5";
 	public static final String GEMINI_API_ENDPOINT = "https://generativelanguage.googleapis.com/v1/models/";
