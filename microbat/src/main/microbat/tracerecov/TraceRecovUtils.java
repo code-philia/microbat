ce4066d617036c89ba38ef372304afe5a5e57631
hongshu <hongshu.wang.uni@gmail.com>
2025-03-14 00:29:34 +0800

    fix line identification bug in in-context learning

--- microbat/src/main/microbat/tracerecov/TraceRecovUtils.java
+++ microbat/src/main/microbat/tracerecov/TraceRecovUtils.java
@@ -244,7 +244,8 @@ public class TraceRecovUtils {
 			while ((line = reader.readLine()) != null) {
 				line = line.strip();
 				currentLine++;
-				if (line.startsWith("public ") || line.startsWith("private ") || line.startsWith("protected ")) {
+				if (line.contains("(") && (line.startsWith("public ") || line.startsWith("private ")
+						|| line.startsWith("protected "))) {
 					methodStartLine = currentLine;
 				}
 				if (currentLine == lineNumber) {
