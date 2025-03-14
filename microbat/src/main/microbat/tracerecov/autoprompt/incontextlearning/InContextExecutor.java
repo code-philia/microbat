d3b4ce57df4bd6b48d15e3d53577b8d08d9144ad
HongshuW <hongshu.wang.uni@gmail.com>
2025-03-14 22:54:46 +0800

    restore parameters for instrumenter

--- microbat/src/main/microbat/tracerecov/autoprompt/incontextlearning/InContextExecutor.java
+++ microbat/src/main/microbat/tracerecov/autoprompt/incontextlearning/InContextExecutor.java
@@ -21,6 +21,7 @@ import org.slf4j.LoggerFactory;
 import microbat.codeanalysis.runtime.InstrumentationExecutor;
 import microbat.codeanalysis.runtime.StepLimitException;
 import microbat.instrumentation.AgentParams;
+import microbat.instrumentation.CommonParams;
 import microbat.instrumentation.output.RunningInfo;
 import microbat.model.trace.Trace;
 import microbat.util.StringFormatUtils;
@@ -194,10 +195,10 @@ public class InContextExecutor {
                 TRACE_FILE_NAME,
                 includeLibs,
                 excludeLibs);
-//        executor.getAgentRunner().addAgentParam(CommonParams.OPT_FORCE_EXIT_WITHOUT_WAIT_OTHER_THREADS, "true");
-//        executor.getAgentRunner().addAgentParam(CommonParams.OPT_MANUALLY_TEST_RUNNING_CLASS, "SampleTest");
+        executor.getAgentRunner().addAgentParam(CommonParams.OPT_FORCE_EXIT_WITHOUT_WAIT_OTHER_THREADS, "true");
+        executor.getAgentRunner().addAgentParam(CommonParams.OPT_MANUALLY_TEST_RUNNING_CLASS, "SampleTest");
         executor.getAgentRunner().addAgentParam(AgentParams.OPT_VARIABLE_LAYER, Integer.toString(3));
-//        executor.getAgentRunner().addAgentParam(AgentParams.OPT_TRACKING_ALL_FIELDS, "true");
+        executor.getAgentRunner().addAgentParam(AgentParams.OPT_TRACKING_ALL_FIELDS, "true");
         RunningInfo results = null;
         try {
             results = executor.run();
